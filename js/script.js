bd0054c742e1bd381aa1ac46dbe0c3ba214084ae
hamza-aabila <45084155+hamza-aabila@users.noreply.github.com>
2020-11-18 12:26:10 +0100

    delete slide

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-// JavaScript Document
-$('.bxslider').bxSlider({
-  minSlides: 1,
-  maxSlides: 3,
-  slideWidth: 370,
-  slideMargin: 15
-});
-
-
-$(document).ready(function(){
-  $('.slider1').bxSlider({
-    slideWidth: 200,
-    minSlides: 1,
-    maxSlides: 4,
-    slideMargin: 20
-  });
-});
-
- $(document).ready(function() {
+$(document).ready(function() {
 jQuery(document).ready(function( $ ) {
         $('.counter').counterUp({
             delay: 10,
